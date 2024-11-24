77794472c4daa68166067e51bf7530b3cdb17951
mktu <mushi9ui@gmail.com>
2024-11-24 11:02:21 +0900

    fix search in dialog

--- app/(app)/app/bookmarks/_actions/handleFetchBookmarks.ts
+++ app/(app)/app/bookmarks/_actions/handleFetchBookmarks.ts
@@ -1,25 +1,8 @@
 'use server'
-import { fetchBookmarksByPage, getBookmarksByIds, searchBookmarks } from "@/lib/repositories/bookmarks";
+import { getBookmarksByIds, searchBookmarks } from "@/lib/repositories/bookmarks";
 import { createClientForServer } from "@/lib/supabase/supabaseServer";
 import { BookmarkSortOption } from "@/lib/types";
 import { buildQueryData, parseSearchQuery } from "../_utils/parseSearchQuery";
-
-export const handleFetchBookmarks = async (page: number, limit: number, tags: number[] | null, filter?: string, sortOption?: BookmarkSortOption, category?: number | null) => {
-
-    const start = page * limit;
-    const end = start + limit - 1;
-
-    const supabase = createClientForServer();
-    const { data: authData } = await supabase.auth.getUser();
-    if (!authData?.user) {
-        return { error: 'not authenticated' }
-    }
-    const { bookmarks, count } = await fetchBookmarksByPage(supabase, authData.user.id, page, limit, tags, filter, sortOption, category)
-    return {
-        bookmarks: bookmarks,
-        hasMore: count ? count > end : false
-    }
-}
 
 export const handleSearchBookmarks = async (page: number, limit: number, query?: string, sort?: BookmarkSortOption) => {
     const supabase = createClientForServer();
--- app/(app)/app/bookmarks/_components/dialog-content/BookmarkDialogContent.tsx
+++ app/(app)/app/bookmarks/_components/dialog-content/BookmarkDialogContent.tsx
@@ -1,25 +1,44 @@
 'use client'
 import { FC } from "react";
-import { InputWithIcon } from "@/components/domain/InputWithIcon";
-import { SearchIcon } from "lucide-react";
 import { useBookmarkSearch } from "../../_hooks/useBookmarkSearch";
 import BookmarkListItem from "./BookmarkListItem";
+import SearchBox from "../search-box/SearchBox";
+import { CategoryOperator, TagOperator } from "../../_utils/parseSearchQuery";
 
 const BookmarkDialogContent: FC = () => {
-    const { bookmarks, search, setSearch } = useBookmarkSearch()
+    const {
+        bookmarks,
+        search,
+        selectableCategories,
+        selectableTags,
+        isSuggesting,
+        onUpdate,
+        onAddCommand,
+        onSelectSuggestion
+    } = useBookmarkSearch()
     return (
         <div className="flex h-full flex-col gap-2 overflow-y-hidden px-2 py-4">
-            <InputWithIcon
-                placeholder="Search Bookmarks"
-                value={search}
-                leftIcon={<SearchIcon className='size-4 text-muted-foreground' />}
-                className="w-[400px]"
-                onChange={(e) => {
-                    setSearch(e.target.value)
-                }} />
+            <SearchBox
+                searchText={search}
+                onChangeSearchText={onUpdate}
+                onSelectTag={(tag) => {
+                    onSelectSuggestion(TagOperator, tag.name)
+                }}
+                onSelectCategory={(category) => {
+                    onSelectSuggestion(CategoryOperator, category.name)
+                }}
+                onAddCommand={onAddCommand}
+                selectableCategories={selectableCategories}
+                selectableTags={selectableTags}
+                isSuggesting={isSuggesting}
+            />
             <ul className="mt-2 flex flex-1 flex-col gap-2 overflow-y-auto">
                 {bookmarks.map((bookmark) => (
-                    <BookmarkListItem key={bookmark.bookmarkId} bookmark={bookmark} />
+                    <BookmarkListItem key={bookmark.bookmarkId}
+                        onSelectTag={(tag) => {
+                            onSelectSuggestion(TagOperator, tag)
+                        }}
+                        bookmark={bookmark} />
                 ))}
                 {bookmarks.length === 0 && (
                     <p className='w-full p-4 text-lg text-muted-foreground'>Bookmark not found.</p>
