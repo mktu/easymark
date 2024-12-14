5146255b5424e2f4591c87b781ee7783f7fb5f17
mktu <mushi9ui@gmail.com>
2024-12-15 00:38:36 +0900

    fix welcome page

---
+++ app/(app)/app/_components/Welcome/Placeholder.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const Placeholder: FC = () => (
+    <div className="border border-dotted w-[460px] h-[280px] flex items-center justify-center p-4 text-muted-foreground">
+        <p>Displaying information from the entered URL.</p>
+    </div>
+)
+
+export default Placeholder
--- app/(app)/app/_components/Welcome/Welcome.tsx
+++ app/(app)/app/_components/Welcome/Welcome.tsx
@@ -4,8 +4,15 @@ import { Input } from "@/components/ui/input";
 import { FC } from "react";
 import { useFormState } from "react-dom";
 import { AddBookmarkState, handleBookmarkSubmit } from "../../_actions/handleAddBookmark";
-import OgpCard from "@/components/domain/OgpCard";
 import { useBookmarkInput } from "../../hooks/useBookmarkInput";
+import Image from "next/image";
+import OgpImage from "@/components/domain/OgpImage";
+import ErrorIndicator from "../ErrorIndicator/ErrorIndicator";
+import Placeholder from "./Placeholder";
+
+
+const ImageWitdth = 420
+const ImageHeight = Math.floor(ImageWitdth / 1.91)
 
 type Props = {
     user: {
@@ -20,29 +27,37 @@ const Welcome: FC<Props> = ({
     const handleAddBookmarkWithOgp = handleBookmarkSubmit.bind(null, ogp || null)
     const [state, dispatch] = useFormState<AddBookmarkState, FormData>(handleAddBookmarkWithOgp, { error: null })
     return (
-        <form className='w-full' action={dispatch}>
-            <div> Welcome {user.username} !<br /> Lets add first bookmark</div>
-            <div className='flex items-center gap-1'>
-                <Input value={bookmark} onChange={(e) => {
-                    setBookmark(e.target.value)
-                }} id='url' name='url' />
-                <Button disabled={!validBookmark}>Add</Button>
+        <section className="flex flex-col gap-2">
+            <div className='flex w-full flex-col gap-4 p-4 items-center'>
+                <h2 className='flex items-center text-lg font-semibold'>
+                    Welcome {user.username}&apos;s Home. Les&apos;s add first bookmark !
+                </h2>
+                <Image src='/images/welcome.svg' width={200} height={200} alt='welcome' />
             </div>
+            <form className='w-full flex flex-col items-center gap-4 p-4' action={dispatch}>
+                <div className='flex items-center gap-1 w-full md:w-[600px]'>
+                    <Input value={bookmark} onChange={(e) => {
+                        setBookmark(e.target.value)
+                    }} id='url' name='url' />
+                    <Button disabled={!validBookmark}>Add</Button>
+                </div>
+                {ogp ? (
+                    <div className="flex flex-col gap-2 w-full md:w-[600px] items-center">
+                        <OgpImage
+                            url={bookmark}
+                            image={ogp?.image?.url}
+                            alt={ogp?.title}
+                            width={ImageWitdth}
+                            height={ImageHeight} />
+                        <h3 className='font-semibold'>{ogp?.title}</h3>
+                        <p className="flex justify-center w-full md:w-[450px] text-sm text-muted-foreground">{ogp?.description}</p>
+                    </div>
+                ) : <Placeholder />}
 
-            {ogp && (
-                <OgpCard
-                    url={ogp.url}
-                    title={ogp.title}
-                    description={ogp.description}
-                    image={ogp.image?.url}
-                    height={200}
-                    width={200}
-                />
-            )}
-
-            {'error' in state && <div>{state.error}</div>}
-            {'validatedErrors' in state && <div>{state.validatedErrors.url}</div>}
-        </form>
+                {'error' in state && <ErrorIndicator error={state.error || ''} />}
+                {'validatedErrors' in state && <ErrorIndicator error={state.validatedErrors.url} />}
+            </form>
+        </section>
     )
 }
 
